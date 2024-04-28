c637b72e4d31306dd46f94768f32cf08744fff92
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-04-28 10:59:18 +0400

    providers can be declared globally

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from 'src/cats/controllers/cats.controller';
 import { CatsService } from 'src/cats/services/cats.service';
-
+// @Global()
 @Module({
     imports: [],
     controllers: [CatsController],
