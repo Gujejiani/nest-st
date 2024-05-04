7622753b5e8740d180442dd1ecccaf4edbf6bdc4
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-05-04 14:23:46 +0400

    covered module refference

--- src/cats/services/cats.service.ts
+++ src/cats/services/cats.service.ts
@@ -1,4 +1,7 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException, Scope } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotFoundException, OnModuleInit, Scope } from "@nestjs/common";
+import { ModuleRef } from "@nestjs/core";
+import { AnimalsController } from "src/animals/animals.controller";
+import { CommonService } from "src/common/common.service";
 import { Cat } from "src/interfaces/cats.interface";
 
 
@@ -6,9 +9,16 @@ import { Cat } from "src/interfaces/cats.interface";
   scope: Scope.REQUEST,
   durable: true
 })
-export class CatsService {
+export class CatsService implements OnModuleInit {
     private readonly cats: Cat[] = [];
-
+    private service: AnimalsController;
+    constructor(private moduleRef: ModuleRef) {}
+    onModuleInit() {
+      // doesn't fire if score is Request or transient and using durable
+      console.log('cats service initialized')
+      this.service = this.moduleRef.get(AnimalsController);
+      console.log(this.service)
+    }
     create(cat: Cat) {
       this.cats.push(cat);
       return 'cat added'
