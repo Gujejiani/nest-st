8a78b30b1877a1d288df6d3a547949685b55bd9a
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-05-24 23:10:05 +0400

    covered logger part

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -1,15 +1,16 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInterceptor.name);
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('Before...');
 
     const now = Date.now();
      // log response 
-       
+     this.logger.log('Hi from interceptor')
 
     return next
       .handle()
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { AggregateByTenantContextIdStrategy } from './global/contextIdStrategy';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['error', 'warn', 'log'],
+  
+  });
   // global middlware 
  // app.use(middlWare)
 
